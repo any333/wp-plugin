a05527387bbe0aa22db656439f12b5f15e2cf34a
any333 <snickers.bubble@gmail.com>
2013-12-29 17:49:32 +0200

    all of the fields working properly

--- events/events.php
+++ events/events.php
@@ -29,8 +29,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
     function event_custom_post_types() {
 		register_post_type( 'eventbase', array(
 			'labels' => array(
-				'name' => __("Events", 'eventbase'),
-				'singular_name' => __("Event", 'eventbase'),
+				'name' => __("Events", 'eventbase' ),
+				'singular_name' => __("Event", 'eventbase' ),
 				'add_new' => _x("Add New", 'eventbase', 'eventbase' ),
 				'add_new_item' => __("Add New Event", 'eventbase' ),
 				'edit_item' => __("Edit Event", 'eventbase' ),
@@ -65,20 +65,20 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 		register_taxonomy( 'eventbase_taxonomy', 'eventbase', array(
 			'hierarchical' => true,
 			'labels' => array(
-				'name' => _x( "Event Types", 'taxonomy general name', 'dxbase' ),
-				'singular_name' => _x( "Event Type", 'taxonomy singular name', 'dxbase' ),
-				'search_items' =>  __( "Search for a certain type", 'dxbase' ),
-				'popular_items' => __( "Popular Event Types", 'dxbase' ),
-				'all_items' => __( "All Event Types", 'dxbase' ),
+				'name' => _x( "Event Types", 'taxonomy general name', 'eventbase' ),
+				'singular_name' => _x( "Event Type", 'taxonomy singular name', 'eventbase' ),
+				'search_items' =>  __( "Search for a certain type", 'eventbase' ),
+				'popular_items' => __( "Popular Event Types", 'eventbase' ),
+				'all_items' => __( "All Event Types", 'eventbase' ),
 				//'parent_item' => null,
 				//'parent_item_colon' => null,
-				'edit_item' => __( "Edit Event Type", 'dxbase' ), 
-				'update_item' => __( "Update the event type", 'dxbase' ),
-				'add_new_item' => __( "Add the new event type", 'dxbase' ),
-				'new_item_name' => __( "New Event Type Name", 'dxbase' ),
-				'separate_items_with_commas' => __( "Separate types with commas", 'dxbase' ),
-				'add_or_remove_items' => __( "Add or remove types", 'dxbase' ),
-				'choose_from_most_used' => __( "Choose from the most used types", 'dxbase' )
+				'edit_item' => __( "Edit Event Type", 'eventbase' ), 
+				'update_item' => __( "Update the event type", 'eventbase' ),
+				'add_new_item' => __( "Add the new event type", 'eventbase' ),
+				'new_item_name' => __( "New Event Type Name", 'eventbase' ),
+				'separate_items_with_commas' => __( "Separate types with commas", 'eventbase' ),
+				'add_or_remove_items' => __( "Add or remove types", 'eventbase' ),
+				'choose_from_most_used' => __( "Choose from the most used types", 'eventbase' )
 			),
 			'show_ui' => true,
 			'query_var' => true,
@@ -87,66 +87,88 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 		
 		register_taxonomy_for_object_type( 'eventbase_taxonomy', 'eventbase' );
 	}
-
 	
 	
 	add_action( 'add_meta_boxes', 'add_box' );
 	function add_box() {
-		add_meta_box('normal', 'Options', 'display_events_meta_boxes');
+		add_meta_box( 'normal', 'Options', 'display_events_meta_boxes');
 	}
 	
-	function display_events_meta_boxes($display_events){
+	function display_events_meta_boxes( $display_events ){
 
-		$start_time=get_post_meta($display_events->ID, 'start_time', true);
-		$end_time=get_post_meta($display_events->ID, 'end_time', true);
-		$date=get_post_meta($display_events->ID, 'date', true);
-		$repeat=get_post_meta($display_events->ID, 'repeat', true);
-		$frequency=get_post_meta($display_events->ID, 'frequency', true);		
+		$start_time=get_post_meta( $display_events->ID, 'start_time', true );
+		$end_time=get_post_meta( $display_events->ID, 'end_time', true );
+		$date=get_post_meta( $display_events->ID, 'date', true );
+		$repeat=get_post_meta( $display_events->ID, 'repeat', true );
+		$frequency=get_post_meta( $display_events->ID, 'frequency', true );		
 		
 	?>
 	<table>
 		<tr>
 			<td style="width: 40%">Start Time</td>
-			<td><input type="datetime-local" size="40" name="event_start_time" value="<?php echo $start_time; ?>" /></td>
+			<td><input type="datetime-local" size="40" name="event_start_time" value="<?php echo $start_time ?>" /></td>
 		</tr>
 		<tr>
 			<td style="width: 40%">End Time</td>
-			<td><input type="datetime-local" size="40" name="event_end_time" value="<?php echo $end_time; ?>" /></td>
+			<td><input type="datetime-local" size="40" name="event_end_time" value="<?php echo $end_time ?>" /></td>
 		</tr>
 		<tr>
             <td style="width: 40%">Date</td>
-            <td><input type="date" size="40" name="event_date" value="<?php echo $date; ?>" /></td>
+            <td><input type="date" size="40" name="event_date" value="<?php echo $date ?>" /></td>
         </tr>
         <tr>
 			<td style="width: 150px">Repeat</td>
 			<td>
+			<?php $repeated_value = array(
+					'yes' => 'Yes',
+					'no' => 'No'
+			);
+			
+			?>
                 <select style="width: 100px" name="event_repeating">
-                  	<option value="yes">Yes</option>
-  					<option value="no">No</option>
-  					<?php 
-  					$repeat = $_POST['event_repeating'];
-  					echo $repeat;
+                	
+                	<?php 
+                	
+                	foreach ( $repeated_value as $key => $value ) {
+                		?>
+                		 <option value= '<?php echo esc_attr($key)?>' <?php selected( $repeat, $key ); ?>> <?php echo esc_html($value) ?> </option>
+                	<?php 	
+                	}    
+               		
+  					echo selected( $repeat );
   					?>
+  					
                 </select>
             </td>
         </tr>
         <tr>
             <td style="width: 40%">Frequency</td>
             <td>
-                <select style="width: 100px" name="event_frequency">
-                	<option value="none"> </option>
-                  	<option value="daily">dayli</option>
-  					<option value="weekly">weekly</option>
-  					<option value="bi_weekly">bi-weekly</option>
-  					<option value="monthly">monthly</option>
-  					<option value="3_months">3-months</option>
-  					<?php
-					$frequency = $_POST['event_frequency'];
-  					echo $frequency;
-  					?>			
-                </select>
-            </td>
-            
+			<?php 
+			$frequencies = array(
+            		'none' => 'None',
+            		'daily' => 'Daily',
+            		'weekly' => 'Weekly',
+           			'bi-weekly' => 'Bi-Weekly',
+           			'monthly' => 'Monthly',
+           			'3-months' => '3-Months'
+           			);
+   			?> 
+
+   			<select style="width: 100px" name="event_frequency" > 
+   			<?php 
+   			          
+   				foreach ( $frequencies as $key => $value ) {
+      		?> 
+      		      <option value= '<?php echo esc_attr($key)?>' <?php selected( $frequency, $key ); ?>> <?php echo esc_html($value) ?> </option> 
+      		<?php 
+            	}
+            			
+            	echo selected( $frequency );
+            	
+   			?>        
+            	</select>   	                
+            </td>          
         </tr>
     </table>
     <?php
@@ -155,33 +177,32 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 	
 	add_action( 'save_post', 'events_boxes_save', 10, 2 );
 	function events_boxes_save( $post_id, $display_events ) {
-		if ($display_events->post_type == 'eventbase'){
+		
+		if ( $display_events->post_type == 'eventbase' ) {
 			
-			if(isset($_POST ['event_start_time'] )){
-				update_post_meta( $post_id, 'start_time', $_POST['event_start_time']);			
+			if( isset( $_POST ['event_start_time'] ) ) {
+				update_post_meta( $post_id, 'start_time', $_POST['event_start_time'] );			
 			}
-			
 	
-			if(isset($_POST ['event_end_time'] )){
-				update_post_meta( $post_id, 'end_time', $_POST['event_end_time']);			
+			if( isset( $_POST ['event_end_time'] ) ) {
+				update_post_meta( $post_id, 'end_time', $_POST['event_end_time'] );			
 			}
 			
-			if(isset($_POST ['event_date'] )){
-				update_post_meta( $post_id, 'date', $_POST['event_date']);			
+			if( isset( $_POST ['event_date'] ) ) {
+				update_post_meta( $post_id, 'date', $_POST['event_date'] );			
 			}
 		
-			if(isset($_POST ['event_repeating'] )){
-				update_post_meta( $post_id, 'repeat', $_POST['event_repeating']);			
-			}
-			
+			if( isset( $_POST ['event_repeating'] ) ) {
+				update_post_meta( $post_id, 'repeat', $_POST['event_repeating'] );			
+			}		
 		
-			if(isset($_POST ['event_frequency'] )){
-				update_post_meta( $post_id, 'frequency', $_POST['event_frequency']);			
+			if( isset( $_POST ['event_frequency'] ) ) {
+				update_post_meta( $post_id, 'frequency', $_POST['event_frequency'] );			
 			}	
+		
 		}
 		
 	}
 		
-	
 	include 'show_events.php';
 	
--- events/show_events.php
+++ events/show_events.php
@@ -2,7 +2,7 @@
 
 add_action( 'init', 'show_calendar' );
 function show_calendar() {
-	add_shortcode( 'eventts','calendar');
+	add_shortcode( 'eventts', 'calendar');
 }
 
 function calendar() {
@@ -11,8 +11,8 @@ function calendar() {
 						"May", "June", "July", "August", "September", 
 						"October", "November", "December");
 
-	if (!isset($_REQUEST["month"])) $_REQUEST["month"] = date("n");
-	if (!isset($_REQUEST["year"])) $_REQUEST["year"] = date("Y");
+	if ( !isset( $_REQUEST["month"] ) ) $_REQUEST["month"] = date("n" );
+	if ( !isset( $_REQUEST["year"] ) ) $_REQUEST["year"] = date("Y" );
 
 	$cMonth = $_REQUEST["month"];
 	$cYear = $_REQUEST["year"];
@@ -22,63 +22,78 @@ function calendar() {
 	$prev_month = $cMonth-1;
 	$next_month = $cMonth+1;
 
-	if ($prev_month == 0 ) {
+	if ( $prev_month == 0 ) {
 		$prev_month = 12;
 		$prev_year = $cYear - 1;
 	}
-	if ($next_month == 13 ) {
+	
+	if ( $next_month == 13 ) {
 		$next_month = 1;
 		$next_year = $cYear + 1;
 			
 	}
 	?>
-<table width="200">
-	<tr align="center">
-		<td bgcolor="white" >
-		<table width="100%" border="0" cellspacing="" cellpadding="0">
-			<tr>
-				<td width="50%" align="left"><a href="<?php echo $_SERVER["PHP_SELF"] . "?month=". $prev_month . "&year=" . $prev_year; ?>"style="color: black">Previous</a></td>
-				<td width="50%" align="right"><a href="<?php echo $_SERVER["PHP_SELF"] . "?month=". $next_month . "&year=" . $next_year; ?>"style="color: black">Next</a></td>
-			</tr>
-		</table>
-		</td>
-	</tr>
-	<tr>
-		<td align="center">
-		<table width="100%" border="0" cellpadding="2" cellspacing="2">
-			<tr align="center">
-				<td colspan="7" bgcolor="green" style="color: white"><strong><?php echo $monthNames[$cMonth-1].' '.$cYear; ?></strong></td>
-			</tr>
-			<tr>
-				<td align="center" bgcolor="black" style="color: white"><strong>Monday</strong></td>
-				<td align="center" bgcolor="black" style="color: white"><strong>Tuesday</strong></td>
-				<td align="center" bgcolor="black" style="color: white"><strong>Wednesday</strong></td>
-				<td align="center" bgcolor="black" style="color: white"><strong>Thursday</strong></td>
-				<td align="center" bgcolor="black" style="color: white"><strong>Friday</strong></td>
-				<td align="center" bgcolor="black" style="color: white"><strong>Saturday</strong></td>
-				<td align="center" bgcolor="black" style="color: white"><strong>Sunday</strong></td>
-			</tr>
-
+	<table width="200">
+		<tr align="center">
+			<td bgcolor="white" >
+			<table width="100%" border="0" cellspacing="" cellpadding="0">
+				<tr>
+					<td width="50%" align="left"><a href="<?php echo $_SERVER["PHP_SELF"] . "?month=". $prev_month . "&year=" . $prev_year; ?>"style="color: black">Previous</a></td>
+					<td width="50%" align="right"><a href="<?php echo $_SERVER["PHP_SELF"] . "?month=". $next_month . "&year=" . $next_year; ?>"style="color: black">Next</a></td>
+				</tr>
+			</table>
+			</td>
+		</tr>
+		<tr>
+			<td align="center">
+			<table width="100%" border="0" cellpadding="2" cellspacing="2">
+				<tr align="center">
+					<td colspan="7" bgcolor="green" style="color: white"><strong><?php echo $monthNames[$cMonth-1].' '.$cYear; ?></strong></td>
+				</tr>
+				<tr>
+					<td align="center" bgcolor="black" style="color: white"><strong>Monday</strong></td>
+					<td align="center" bgcolor="black" style="color: white"><strong>Tuesday</strong></td>
+					<td align="center" bgcolor="black" style="color: white"><strong>Wednesday</strong></td>
+					<td align="center" bgcolor="black" style="color: white"><strong>Thursday</strong></td>
+					<td align="center" bgcolor="black" style="color: white"><strong>Friday</strong></td>
+					<td align="center" bgcolor="black" style="color: white"><strong>Saturday</strong></td>
+					<td align="center" bgcolor="black" style="color: white"><strong>Sunday</strong></td>
+				</tr>
+	
+				<?php
+				$timestamp = mktime( 0, 0, 0, $cMonth, 1, $cYear );
+				$maxday = date( "t", $timestamp );
+				$thismonth = getdate ( $timestamp );
+				$startday = $thismonth['wday'] - 1;
+				if ( $startday == -1 )
+				$startday = 6;
+				
+				for ( $i=0; $i<($maxday+$startday); $i++ ) {
+					if( ( $i % 7 ) == 0 ) echo "<tr>";
+					if( $i < $startday ) echo "<td></td>";
+					else echo "<td align='center' valign='middle' height='80px'>". ( $i - $startday + 1 ) . "</td>";
+					if( ( $i % 7 ) == 6 ) echo "</tr>";
+				}
+				?>
+	
+			</table>
+			</td>
+		</tr>
+	</table>
 			<?php
-			$timestamp = mktime(0,0,0,$cMonth,1,$cYear);
-			$maxday = date("t",$timestamp);
-			$thismonth = getdate ($timestamp);
-			$startday = $thismonth['wday'] - 1;
-			if ($startday == -1)
-			$startday = 6;
+			/*
+			function get_from_db() {
 			
-			for ($i=0; $i<($maxday+$startday); $i++) {
-				if(($i % 7) == 0 ) echo "<tr>";
-				if($i < $startday) echo "<td></td>";
-				else echo "<td align='center' valign='middle' height='80px'>". ($i - $startday + 1) . "</td>";
-				if(($i % 7) == 6 ) echo "</tr>";
+				global $wpdb;
+			
+				$show_post_id = $wpdb->get_var( "SELECT meta_value, post_id, meta_key FROM wp_postmeta where post_id = '131' and meta_key= 'repeat' ");
+		
+				echo $show_post_id;
+	
 			}
-			?>
-
-		</table>
-		</td>
-	</tr>
-</table>
-			<?php
-
+			
+			get_from_db();*/
 }
+
+
+		
